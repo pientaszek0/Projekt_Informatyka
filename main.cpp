675b72d2abc97f86eeb1a87c2e3cbbec4dce0452
Michalolosgit <michawierzbicki1@gmail.com>
2024-10-16 20:23:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,38 @@
 #include "variables.h"
 using namespace std;
 
+// Funkcja obliczająca miesięczną ratę kredytu
+double obliczRateKredytu(double kwotaKredytu, double stopaRoczna, int liczbaLat) {
+    // Przekształcamy stopę roczną na miesięczną
+    double stopaMiesieczna = stopaRoczna / 12 / 100;
+    // Liczba miesięcy
+    int liczbaMiesiecy = liczbaLat * 12;
+    
+    double rata = (kwotaKredytu * stopaMiesieczna * pow(1 + stopaMiesieczna, liczbaMiesiecy)) / (pow(1 + stopaMiesieczna, liczbaMiesiecy) - 1);
+    
+    return rata;
+}
+
 int main() {
-    cout << "Hello World!";
+    double kwotaKredytu;
+    double stopaRoczna;
+    int liczbaLat;
+    
+    // Pobieranie danych od użytkownika
+    cout << "Podaj kwotę kredytu: ";
+    cin >> kwotaKredytu;
+
+    cout << "Podaj roczną stopę procentową (w %): ";
+    cin >> stopaRoczna;
+
+    cout << "Podaj liczbę lat kredytu: ";
+    cin >> liczbaLat;
+
+    // Obliczenie i wyświetlenie miesięcznej raty
+    double rata = obliczRateKredytu(kwotaKredytu, stopaRoczna, liczbaLat);
+    cout << "Miesięczna rata kredytu wynosi: " << rata << std::endl;
+
     return 0;
 }
+
  
