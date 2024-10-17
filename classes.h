8ce28afb56553c143371f916020acee90f4afda8
Michalolosgit <michawierzbicki1@gmail.com>
2024-10-17 10:27:52 +0200

    Update classes.h

--- classes.h
+++ classes.h
@@ -34,22 +34,12 @@ class Loan_Type {
 
     public:
         // Konstruktor
-        Loan_Type(int id, double interest, string loan_type) : id(id), interest(interest), loan_type(loan_type) {}
+        Loan_Type(int id, double interest, string loan_type);
 
-        // Funkcja zwracająca stopę procentową
-        double getInterest() const {
-            return interest;
-        }
-
-        // Funkcja zwracająca nazwę typu kredytu
-        string getLoanTypeName() const {
-            return loan_type;
-        }
-
-        // Funkcja obliczająca całkowite odsetki na podstawie kwoty kredytu i lat
-        double calcInterest(double loanAmount, int years) const {
-            return loanAmount * interest * years / 100; // Prosty wzór na odsetki
-        }
+        //Funkcje
+        double getInterest() const;
+        string getLoanTypeName() const;
+        double calcInterest(double loanAmount, int years) const;
 };
 
 // Klasa kredytów zaciągniętych przez użytkowników
@@ -61,31 +51,11 @@ class Loan {
 
     public:
         // Konstruktor
-        Loan(int owner_id, Lone_type loane_type, int currency_id, double balance_left) :
-            owner_id(owner_id), loan_type(loan_type), currency_id(currency_id), balance_left(balance_left) {}
+        Loan(int owner_id, Lone_type loane_type, int currency_id, double balance_left);
 
-        // Funkcja do spłaty części kredytu
-        void makePayment(double payment) {
-            if (payment > 0 && payment <= balance_left) {
-                balance_left -= payment;
-                cout << "Spłacono " << payment << ". Pozostało do spłaty: " << balance_left << endl;
-            } else {
-                cout << "Nieprawidłowa kwota płatności." << endl;
-            }
-        }
-
-        // Funkcja zwracająca ile pozostało do spłacenia
-        double getBalanceLeft() const {
-            return balance_left;
-        }
-
-        // Funkcja zwracająca ID właściciela
-        int getOwnerId() const {
-            return owner_id;
-        }
-        
-        // Funkcja zwracająca typ kredytu
-        string getLoanType() const {
-            return loan_type.getLoanTypeName();
-        }
+        // Funkcje
+        void makePayment(double payment);
+        double getBalanceLeft() const;
+        int getOwnerId() const;
+        string getLoanType() const;
 };
