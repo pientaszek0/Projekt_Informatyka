df49972c3b1d402473f4f08baea055ccc5596426
Prymasek <adrian.prym96@gmail.com>
2024-10-17 10:52:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "classes.h"
-#include "functions.h"
-#include "variables.h"
+//#include "classes.h"
+//#include "functions.h"
+//#include "variables.h"
 using namespace std;
 
 
 int main() {
-
+    cout<<"sshgs";
     return 0;
 }
 
