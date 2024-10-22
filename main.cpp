fb99c9e285762535372ab85dc13dae026f253fc9
Jan Piętka <pientaszek007@gmail.com>
2024-10-22 16:44:59 +0200

    funkcja miała złe argumenty

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main() 
 {
     xml_chf(user); // Wywołanie funkcji do odczytu pliku xml 
-    xml_giveData(user, account, currency); // Wywołanie funkcja która wczytuje dane z XML
+    xml_giveData(user, account, currency, loan_type); // Wywołanie funkcja która wczytuje dane z XML
     xml_checkData(user, account, currency); // Wywołanie funkcji sparwdza poprawność czy dane zostały dobrze wczytane !!TYLKO "User!!"
 
     while (true) { // Nieskonczona petla zeby mozna bylo sie wylogowac i zalogowac na innego uzytkownika
